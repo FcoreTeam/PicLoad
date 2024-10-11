b1c58540dfe38722dce98893c9d78c815f8a3ee3
FcoreTeam <fcoreteam@mail.ru>
2024-10-11 21:41:52 +0300

    added categories, added part of popup, gradient

--- front-end/src/components/pages/main/Main.jsx
+++ front-end/src/components/pages/main/Main.jsx
@@ -1,10 +1,15 @@
-import styles from "./main.module.scss";
+import Categories from "./categories/Categories";
 import Profile from "./profile/Profile";
+
+import styles from "./main.module.scss";
+import MemoryWarning from "../popups/memory-warning/Memory-warning";
 
 const Main = ({ state }) => {
   return (
     <main className={styles.main__page}>
+      {false && <MemoryWarning />}
       <Profile state={state.profilePage} />
+      <Categories state={state.photoPage} />
     </main>
   );
 };
---
+++ front-end/src/components/pages/popups/memory-warning/Memory-warning.jsx
@@ -0,0 +1,12 @@
+import Popup from "../Popup";
+
+import styles from "./memory-warning.module.scss";
+
+const MemoryWarning = () => {
+  return (
+    <Popup>
+
+    </Popup>
+  );
+};
+export default MemoryWarning;
