919ffcf5d6f9c2323e8f66a6ae50ed9d73ca9599
ashmuradyann <69338957+ashmuradyann@users.noreply.github.com>
2024-10-12 16:17:35 +0300

    vite version of the app, some fixes

---
+++ vite/src/components/pages/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MemoryWarning from "./popups/memory-warning/Memory-warning";
+
+const Page = ({children}) => {
+  return (
+    <>
+      {children}
+      {/* navbar сюда */}
+      {!true && <MemoryWarning />}
+    </>
+  )
+}
+
+export default Page
