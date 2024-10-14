624ca36929743d95127a8f2b84db7807d0f36edc
Bogd4nB1 <sindbad.long@yandex.ru>
2024-10-14 15:18:31 +0300

    Changed routing

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,6 +1,5 @@
 import { client } from '../config/database.js';
 import { errorLogStream } from '../app.js';
-import coupon from 'coupon-code';
 import { bot } from '../bot/bot.js';
 
 // GET query
@@ -48,45 +47,6 @@ export const getCategory = async (req, res) => {
         errorLogStream.write(`Error while fetching categories: ${err.message}\n`);
         res.json({error: 'Error while fetching categories'})
     })
-}
-
-// POST query
-
-export const addPromo = async (req, res) => {
-    // POST create new promo
-    try {
-        const couponCode = coupon.generate({ parts: 3 });
-        await client.query(`INSERT INTO promo (code, discount) VALUES ($1, $2)`, [couponCode, req.body.discount]);
-        res.json({status: 'Promocode was added', code: couponCode, discount: req.body.discount});
-    } catch (err) {
-        console.log(err);
-        errorLogStream.write(`Error while adding promo: ${err.message}\n`);
-        res.json({error: 'Error while adding promo'})
-    }
-}
-
-export const addCatOfUsers = async (req, res) => {
-    // POST
-    var info = await client.query('INSERT INTO cat_of_user (user_tg_id, category_id, quantity) VALUES ($1, $2, $3)', [req.body.user_tg_id, req.body.category_id, req.body.quantity]);
-    info.then((data)=>{
-        res.json(data);
-    }).catch((err)=>{
-        console.log(err);
-        errorLogStream.write(`Error while adding category of users: ${err.message}\n`);
-        res.json({error: 'Error while adding category of users'})
-    });
-}
-
-export const addCategory = async (req, res) => {
-    // POST
-    var info = await client.query('INSERT INTO category (title) VALUES ($1)', [req.body.title]);
-    info.then((data)=>{
-        res.json(data);
-    }).catch((err)=>{
-        console.log(err);
-        errorLogStream.write(`Error while adding category: ${err.message}\n`);
-        res.json({error: 'Error while adding category'})
-    });
 }
 
 export const updateCatOfUsers = async (req, res) => {
--- back-end/routes/router.js
+++ back-end/routes/router.js
@@ -1,15 +1,11 @@
 import { Router } from "express";
-import { getCatOfUser, getUserInfo, getCategory, updateCatOfUsers, addCatOfUsers, addCategory, addPromo } from "../controllers/user.js";
+import { getCatOfUser, getUserInfo, getCategory, updateCatOfUsers } from "../controllers/user.js";
 
 const router = Router();
 
 router.get('/api/', getUserInfo)
 router.get('/api/categoryofuser', getCatOfUser)
 router.get('/api/category', getCategory)
-
-router.post('/api/addcategory', addCategory) 
-router.post('/api/addcatofuser', addCatOfUsers)
-router.post('/api/addpromo', addPromo)
 
 router.put('/api/updatecatofusers', updateCatOfUsers)
 
