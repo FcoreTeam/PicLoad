42737759adfcd3e1c1e64a10646972ab83bce833
Bogd4nB1 <sindbad.long@yandex.ru>
2024-10-15 20:37:06 +0300

    Changed functions in user.js

--- back-end/config/initCreateAllTables.js
+++ back-end/config/initCreateAllTables.js
@@ -1,8 +1,9 @@
-import { createUser, createCategory, createCatOfUser, cratePromo } from '../config/sqlquery.js';
+import { createUser, createCategory, createCatOfUser, createPromo, createDefaultCategory } from '../config/sqlquery.js';
 
 export function initCreateAllTables(client) {
     client.query(createUser);
     client.query(createCategory);
     client.query(createCatOfUser);
-    client.query(cratePromo);
+    client.query(createPromo);
+    client.query(createDefaultCategory).catch((err) => {});
 }
