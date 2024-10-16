3c81b8a312593adf8487c4219dc29946c32f99a1
ashmuradyann <69338957+ashmuradyann@users.noreply.github.com>
2024-10-16 20:03:57 +0300

    new requests file

--- front-end/src/api/requests.js
+++ front-end/src/api/requests.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: 'https://api.example.com', // Replace with your API's base URL
-  timeout: 10000, // Optional: Set a timeout for requests
+  baseURL: 'https://api.example.com',
+  timeout: 10000,
 });
 
 export const getProfileData = async (tgId) => await api.get(`http://localhost:3000/api/?tg_id=${tgId}`)
